3b54c3f40b711d790f3f6fa77506fad4f0e657b3
Viktor <komvik.slack@gmail.com>
2024-11-26 12:10:44 +0100

    Make widget AppScreen

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,21 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: AppScreen(),
+    );
+  }
+}
+
+class AppScreen extends StatelessWidget {
+  const AppScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Te'),
       ),
     );
   }
