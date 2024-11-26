8841341191cc050d05a933f71fa50a67f1bc38ec
Viktor <komvik.slack@gmail.com>
2024-11-26 12:21:50 +0100

    text and button are ready

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,30 @@ class AppScreen extends StatelessWidget {
     super.key,
   });
 
+  final String dataStr = " Ts 542";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ts 542"),
+        titleTextStyle: const TextStyle(
+            fontSize: 24, color: Colors.black, fontWeight: FontWeight.bold),
+      ),
       body: Center(
-        child: Text('Te'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 320,
+              child: Text(
+                dataStr,
+                style: const TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(onPressed: () {}, child: const Text(" ABFRAGE ")),
+          ],
+        ),
       ),
     );
   }
